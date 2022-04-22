61c39cc65d5e1fbeadd8ce5d04acbd4ecf3a9052
TzuWei <ap9940506@gmail.com>
2022-04-22 12:38:55 +0800

    add hw2

--- hw2/code.py
+++ hw2/code.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
-
-image = np.array(cv2.imread("input.jpeg", cv2.IMREAD_GRAYSCALE))
+from matplotlib import pyplot as plt
 
 d_2 = np.array([
     [0, 128, 32, 160],
@@ -15,6 +14,9 @@ d_1 = np.array([
 ])
 
 # (A)
+
+image = np.array(cv2.imread("input.jpeg", cv2.IMREAD_GRAYSCALE))
+
 D = d_2
 while D.shape[0] < image.shape[0]:
     D = np.vstack((D, D))
@@ -28,15 +30,20 @@ for i in range(image.shape[0]):
 cv2.imwrite('output-a.jpeg', image)
 
 # (B)
+
+image = np.array(cv2.imread("input.jpeg", cv2.IMREAD_GRAYSCALE))
+
 D = d_1
 while D.shape[0] < image.shape[0]:
     D = np.vstack((D, D))
 while D.shape[1] < image.shape[1]:
     D = np.hstack((D, D))
 
+arr = []
 for i in range(image.shape[0]):
     for j in range(image.shape[1]):
-        q = image[i][j] / (255 / 3)
-        image[i][j] = 255 if image[i][j] > D[i][j] else 0
+        q = int(image[i][j] / (255 / 3))
+        image[i][j] = q + (1 if image[i][j] - 85*q > D[i][j] else 0)
+        image[i][j] = image[i][j]/4 * 255
 
 cv2.imwrite('output-b.jpeg', image)
